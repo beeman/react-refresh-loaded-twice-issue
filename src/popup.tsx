b520e425adfc0e94ba8a7abc62764845ff390de7
Bram Borggreve <beeman@beeman.nl>
2024-04-21 01:04:32 +0400

    repro for issue https://github.com/aklinker1/vite-plugin-web-extension/issues/136

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Popup from "./pages/Popup";
+import {Providers} from "./providers";
 
 ReactDOM.createRoot(document.body).render(
   <React.StrictMode>
-    <Popup />
+      <Providers>
+            <Popup />
+      </Providers>
   </React.StrictMode>
 );
